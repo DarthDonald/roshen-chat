229b34c4924b320092e1ef93e4420120b8cbd256
Andrew <andreychopko@gmail.com>
2017-04-08 22:04:53 +0300

    back upd

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
 
-    resources :products, only: [:index, :show]
-
     resource :user, only: [:create, :update]
-    match 'profile/balance' => "users#update", via: :patch
 
     resource :session, only: [:create, :destroy]
 
-    resources :purchases, only: [:index, :show, :create, :destroy]
-    match '/purchases/drop' => 'purchases#destroy', via: :post
-
-    resources :orders, only: [:index, :show, :create, :update]
-    match '/orders/:id/payment' => 'orders#update', via: :post
-
-    resources :gift_certificates
-    match '/gift_certificates/generate' => 'gift_certificates#generate', via: :post
   end
 
 end
